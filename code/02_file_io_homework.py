42832e5c44d540423fa0eb36f3299bbbbd918085
Kevin Markham <justmarkham@gmail.com>
2014-12-17 14:59:27 -0500

    add class 2 homework assignment file

---
+++ code/02_file_io_homework.py
@@ -0,0 +1,67 @@
+'''
+HOMEWORK: Reading and Writing Files in Python
+'''
+
+'''
+PART 1:
+Read in drinks.csv
+Store the header in a list called 'header'
+Store the data in a list of lists called 'data'
+Hint: you've already seen this code!
+'''
+
+
+
+'''
+PART 2:
+Isolate the beer_servings column in a list of integers called 'beers'
+Hint: you can use a list comprehension to do this in one line
+Expected output:
+    beers == [0, 89, ..., 32, 64]
+    len(beers) == 193
+'''
+
+
+
+'''
+PART 3:
+Create separate lists of NA and EU beer servings: 'NA_beers', 'EU_beers'
+Hint: you can use a list comprehension with a condition
+Expected output:
+    NA_beers == [102, 122, ..., 197, 249]
+    len(NA_beers) == 23
+    EU_beers == [89, 245, ..., 206, 219]
+    len(EU_beers) == 45
+'''
+
+
+
+'''
+PART 4:
+Calculate the average NA and EU beer servings to 2 decimals: 'NA_avg', 'EU_avg'
+Hint: don't forget about data types!
+Expected output:
+    NA_avg == 145.43
+    EU_avg == 193.78
+'''
+
+
+
+'''
+PART 5:
+Write a CSV file called 'avg_beer.csv' with two columns and three rows.
+The first row is the column headers: 'continent', 'avg_beer'
+The second and third rows contain the NA and EU values.
+Hint: think about what data structure will make this easy
+Expected output (in the actual file):
+    continent,avg_beer
+    NA,145.43
+    EU,193.78
+'''
+
+
+
+'''
+BONUS:
+Learn csv.DictReader() and use it to redo Parts 1, 2, and 3.
+'''
