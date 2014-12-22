44b82ba062afa0b2530963170ed1bbafcc57bbd6
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2014-12-22 17:59:13 -0500

    added slides and code for class 3

---
+++ code/03_getting_data.py
@@ -0,0 +1,121 @@
+import requests       # how python goes onto the internet!
+import tweepy       # python wrapper for twitter api
+import json         # javascript object notation
+import time         # timey wimey stuff
+import re           # regex
+from BeautifulSoup import BeautifulSoup # a python HTML parser
+
+# this is my own personal twitter api information
+# if you could be so kind as to sign up yourself on both twitter and mashape that'd be great :)
+# It's FREEEEEEE
+api_key = 'IWIQwPUREMNuQHgZii351TRSq'
+api_secret = 'i1E4MSvMEifjeBTp1hxxAT3RTOTFHdRJhRTWyBBXjWWqPgW2RN'
+access_token = '532821167-m3HQCKb8iKjWpyB2u0KoeIuJTsJNvxSapqGA0wdd'
+access_secret = '0HsqZ3lXcWvhmPWiSvrz8t8aMHcdSnvARbH6OPYy9Dcm7'
+mashape_key = '0CLvblsJOxmshWkaep4szo9CILOMp1PM3hhjsnDi4k8g8ME14o'
+
+
+
+'''
+Example of NO API, we will use requests and BeautifulSoup
+'''
+
+def getStockPrice(symbol): # symbol could be aapl, nflx, ge, etc
+    r = requests.get('http://finance.google.com/finance?q='+'-----') # the url of the google finance page goes in here
+    b = BeautifulSoup(r.text) # create a beautifulsoup object
+    #print b.prettify() # will print the html nicely
+    re_tag = re.compile("-----") #this tag finds the tag with the price in it!!!!
+    span_tag = b.find('span', attrs={'id': re_tag}) 
+    # use find to return THE ONE AND ONLY span tags with an id that matches our regex
+    # use findAll to find all matches
+    quote = span_tag.text # get the text of the tag
+    return quote
+
+print getStockPrice('aapl')
+
+
+'''
+Example of API but NO WRAPPER
+there is no wrapper for this API, so I use requests, a python module for accessing web content
+to access the information
+in a sense, this is a wrapper that I just wrote for the Natural Language Processing API!
+'''
+
+def get_sentiment(phrase):
+    response = requests.post("https://japerk-text-processing.p.mashape.com/sentiment/",
+    headers={
+	    "X-Mashape-Key": mashape_key,
+	    "Content-Type": "application/x-www-form-urlencoded"
+	  },
+	  data={
+	    "language": "english",
+	    "text": '-----'
+	    }
+	)
+    return json.loads(response.text)
+
+print get_sentiment("I really love this song!")
+
+
+
+'''
+Example of API WITH WRAPPER
+tweepy is the python wrapper for twitter data
+'''
+
+
+def getTweetsOnTag(tag):
+	# Documentation is your friend!
+	auth = tweepy.OAuthHandler(api_key, api_secret)
+	auth.set_access_token(access_token, access_secret)
+	api = tweepy.API(auth) # returns a tweepy authorization handler object
+	for tweet in api.search(------): 
+      # other arguments exist, CHECK DOCUMENTATION
+		# print tweet # is a BIG dictionary!!
+		print tweet.created_at, tweet.text
+		
+		# http://docs.tweepy.org/en/v3.1.0/
+
+
+
+getTweetsOnTag("starbucks")
+
+
+
+
+
+'''
+THE BELOW CODE IS OPTIONAl
+It creates a stream of a given tag!
+'''
+
+# This is the listener, resposible for receiving data
+# I will not be covering this in class
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
+        #print decoded
+        time_ =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(decoded['timestamp_ms']) / 1000))
+        handle = decoded['user']['screen_name']
+        tweet_text = decoded['text'].encode('ascii', 'ignore')
+        num_followers = int(decoded['user']['followers_count'])
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        print '@%s at %s: %s with %d followers' % (handle, time_, tweet_text, num_followers)
+        sentiment = "-----" # make this the sentiment from above!!!        
+        print ''
+        
+        return True
+    def on_error(self, status):
+        print status
+def begin_live_feed(tags_to_follow):
+	print "beginning live feed...."
+	l = StdOutListener()
+	auth = tweepy.OAuthHandler(api_key, api_secret)
+	auth.set_access_token(access_token, access_secret)
+	stream = tweepy.Stream(auth, l)
+	stream.filter(track=tags_to_follow)
+begin_live_feed(['@ga', '@ga_ny', '@ga_dc'])
+# this is an example use, if you create a list of words and phrases, 
+# a live stream of tweets about them will show up
+
---
+++ code/03_re_example.py
@@ -0,0 +1,48 @@
+"""
+This is an intro to regular expressions
+
+I use https://regex101.com/#python to check my work!
+"""
+
+
+import re
+
+# flow:
+# create a re pattern object
+# search (or match) it against text
+# orgnize the captures patterns in groups
+
+# \d matches a number
+text = "Hello! My name is Sinan. It is 2014 and it's amazing."
+pattern1 = re.compile("\d")
+re.search(pattern1, text) # == a search object
+# use group to get each instance in the regular expression
+# \d is just ONE number, so it only finds the "2" in "2014"
+re.search(pattern1, text).group(0)
+
+
+# adding a + means "at least one" but potentially more
+pattern2 = re.compile("\d+")
+re.search(pattern2, text).group(0) # == '2014'
+
+# use square brackets [] to match one of the items present
+alphabet = 'abcdefg'
+pattern3 = re.compile('[cfg]')
+re.search(pattern3, alphabet).group(0)
+
+mystery_pattern = re.compile("\d+-\d+-\d+")
+# take a few minutes, and discuss, what application could these myster_patterns have
+re.search(mystery_pattern, "my phone number is 609-462-6706 dude").group(0)
+
+
+# . matches ANYTHING
+all_of_the_text = "dmzhvbekuhvbc     dfljghwco87rc6geinsr6t4gi7rgwefiuvbekuhvbdfljghwco87rc6geinsr6t4gi7rgwefiu ywgsfybcstzvgbrtybte"
+anything_pattern = re.compile(".+")
+re.search(anything_pattern, all_of_the_text).group(0)
+
+# \w matches any word character, alphanumeric
+# if you want to match an actual period, do \.
+email_pattern = re.compile("--------")
+# take a few minutes, and discuss, what application could these myster_patterns have
+re.search(mystery_pattern, "my email address is sinan.u.ozdemir@gmail.com").group(0)
+
