4460bd4dad90e23f53dcc30118798981105653f2
Kevin Markham <justmarkham@gmail.com>
2015-01-07 15:27:36 -0500

    remove part of pandas code for class

--- code/05_pandas.py
+++ code/05_pandas.py
@@ -26,8 +26,6 @@ Reading Files, Summarizing, Selecting, Filtering, Sorting, Detecting Duplicates
 pd.read_table('https://raw.githubusercontent.com/justmarkham/DAT4/master/data/u.user')
 
 # read 'u.user' into 'users'
-u_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
-users = pd.read_table('../data/u.user', header=None, sep='|', names=u_cols, index_col='user_id', dtype={'zip_code':str})
 
 # examine the users data
 users                   # print the first 30 and last 30 rows
@@ -93,42 +91,26 @@ EXERCISE: Working with drinks data
 '''
 
 # Read drinks.csv into a DataFrame called 'drinks' (use the default index)
-drinks = pd.read_table('../data/drinks.csv', sep=',')
-drinks = pd.read_csv('../data/drinks.csv')              # equivalent
 
 # Print the first 10 rows
-drinks.head(10)
 
 # Examine the data types of all columns
-drinks.dtypes
-drinks.info()
 
 # Print the 'beer_servings' Series
-drinks.beer_servings
-drinks['beer_servings']
 
 # Calculate the average 'beer_servings' for the entire dataset
-drinks.describe()                   # summarize all numeric columns
-drinks.beer_servings.describe()     # summarize only the 'beer_servings' Series
-drinks.beer_servings.mean()         # only calculate the mean
 
 # Print all columns, but only show rows where the country is in Europe
-drinks[drinks.continent=='EU']
 
 # Calculate the average 'beer_servings' for all of Europe
-drinks[drinks.continent=='EU'].beer_servings.mean()
 
 # Only show European countries with 'wine_servings' greater than 300
-drinks[(drinks.continent=='EU') & (drinks.wine_servings > 300)]
 
 # Determine which 10 countries have the highest 'total_litres_of_pure_alcohol'
-drinks.sort_index(by='total_litres_of_pure_alcohol').tail(10)
 
 # Determine which country has the highest value for 'beer_servings'
-drinks[drinks.beer_servings==drinks.beer_servings.max()].country
 
 # Count the number of occurrences of each 'continent' value and see if it looks correct
-drinks.continent.value_counts()
 
 
 '''
